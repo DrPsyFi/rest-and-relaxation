643db463f4a282d5a4c7d54f486f7b020370dc1b
Wes Reid <bwreid@gmail.com>
2018-04-05 11:37:24 -0700

    Add boilerplate code and readme.md

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const moment = require('moment')
+const shortid = require('shortid')
+
+app.use(require('morgan')('dev'))
+app.use(require('body-parser').json())
+
+const dreams = [
+  {
+    id: 'SyD01JNif',
+    description: 'I was in class and had forgotten we had a project due! My instructor was upset with me.',
+    date: '18-03-23'
+  },
+  {
+    id: 'SyXAJ1Ejz',
+    description: 'I was walking through a quiet forest, with my two best friends. It was peaceful!',
+    date: '18-04-02'
+  }
+]
+
+const people = [
+  { id: 'r1JRJ1Eoz', name: 'Ms. Weber', relationship: 'Instructor', dreamIds: [ 'SyD01JNif' ] },
+  { id: 'BJgJlkVif', name: 'Ollie', relationship: 'Friend', dreamIds: [ 'SyXAJ1Ejz' ] },
+  { id: 'r1ypy1NoM', name: 'Shari', relationship: 'Friend', dreamIds: [ 'SyXAJ1Ejz' ] }
+]
+
+app.get('/alldreams', (req, res, next) => {
+  res.json({ dreams })
+})
+
+app.get('/allpeople', (req, res, next) => {
+  res.json({ people })
+})
+
+app.use((err, req, res, next) => {
+  const status = 500
+  res.status(status).send(`Something went wrong.`)
+})
+
+app.use((req, res, next) => {
+  res.status(404).send(`Not found.`)
+})
+
+const listener = () => console.log(`Server is running on port ${port}. ✨`)
+app.listen(port, listener)
--- readme.md
+++ readme.md
@@ -1,3 +1,31 @@
 # REST & Relaxation
 
 Example API. Convert to a RESTful architecture.
+
+## Getting Started
+
+1. Fork/clone this repository
+1. `npm install`
+1. `npm start`
+
+## Guiding Questions
+
+* What is the [shortid](https://www.npmjs.com/package/shortid) package? How can you create a new shortid?
+
+* What is the [moment](https://www.npmjs.com/package/moment) package? What will the following code do?
+    ```js
+    const moment = require('moment')
+    moment().format('YYYY-MM-DD')
+    ```
+
+* Try hitting the `/alldreams` route. What is the top level key of the JSON response and where is it set?
+
+## Next Steps
+
+- [ ] Make our two existing `GET` routes more RESTful
+- [ ] Create a **SHOW** route for each resource with an appropriate status code
+- [ ] Create a **DESTROY** route for each resource with an appropriate status code
+- [ ] Create a **SHOW** route for the path `/dreams/:id/people` that only returns the people with the associated dream
+- [ ] Update the custom error handler to return an appropriate status code and helpful content
+- [ ] Add a **CREATE** route for each resource and return an appropriate status code
+- [ ] Update the **CREATE** routes so that they validate for all fields except for ID and, in the case of **dreams**, people; return the correct status code if it's invalid
